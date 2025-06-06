1059647f64dcf564a70438bff53a0ef899a16b91
ThomasRog3rs <Mr.Thomas.Rogers@proton.me>
2025-06-06 20:16:59 +0100

    Init commit

---
+++ BrowserFileManger/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BrowserFileManger.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
